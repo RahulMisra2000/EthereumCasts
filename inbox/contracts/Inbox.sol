936af4e3c4fa0608625eb1223850fb90dbeaee99
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-18 07:14:02 -0400

    Comments

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.4.17;
 
 contract Inbox {
+    
+    // Anytime you create a public class level variable, Solidity automatically gives you
+    // a getter method for that variable. The name of the getter method is the same as the variable name
+    
     string public message;
 
     function Inbox(string initialMessage) public {
