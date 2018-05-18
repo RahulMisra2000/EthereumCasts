f0de4ed600d5091433eb27505c7a81ef68947477
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-18 07:18:10 -0400

    More comments

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,3 +1,5 @@
+// This specifies which version of the Solidity compiler to use when compiling this Smart Contract
+// Notice a smart contract is just like a class in OOP languages
 pragma solidity ^0.4.17;
 
 contract Inbox {
@@ -7,10 +9,15 @@ contract Inbox {
     
     string public message;
 
+    // This is a getter method because it just returns something and does not affect the class level variables
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
     
+    // This is a setter function since it affects the class level variable.
+    // The smart transaction code and the class level variables ARE STORED IN A BLOCK in a BLOCKCHAIN
+    // So a setter method affects the Blockchain ... and to affect the blockchain one has to SEND a Transaction object
+    // so, when calling this method, the caller will also need to send a transaction object along...
     function setMessage(string newMessage) public {
         message = newMessage;
     }
