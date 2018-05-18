499de88cc66923a15f766b535f3d2b40a066ddca
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-17 22:50:16 -0400

    Comments

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,6 +1,10 @@
 // advantage of path is that it is cross platform.... this will work on any nodejs platform
 const path = require('path');
 const fs = require('fs');
+
+// This is the solidity compiler. It returns, in a large object, both the compiled bytecode
+// as well as the Interface that javascript can use to talk to the Smart Contract after it has been
+// deployed to Ethereum network.
 const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
@@ -10,6 +14,8 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 // therefore it needs to be read...
 const source = fs.readFileSync(inboxPath, 'utf8');
 
+
 // We don't have to do .contracts[:Inbox] .....  if you do console.log(solc.compile(source, 1)); 
-// you will see why we are doing it ... because we are just interested in part of the entire LARGE OBJECT
+// you will see why we are doing it ... because we are just interested in part of the entire LARGE OBJECT returned by 
+// solc.compile(source, 1)
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
