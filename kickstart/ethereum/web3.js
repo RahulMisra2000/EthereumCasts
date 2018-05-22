34bab096c3fd3ecd449ce94d9d8b4f5f1637bcd4
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-22 00:09:48 -0400

    Comments

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -4,9 +4,11 @@ let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running.
+  // So, let's use the Provider that NetMask uses
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server *OR* the user is not running metamask
+  // So, we use the HttpProvider
   const provider = new Web3.providers.HttpProvider(
     'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
   );
