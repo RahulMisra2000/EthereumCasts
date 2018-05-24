064fc3be6c845b427b0f072909054187c4c4b93f
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-24 15:14:52 -0400

    Comment

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -5,12 +5,16 @@ const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
+// Campaign.sol contains the CampaignFactory contract and the Campaign contract
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
+
+// Compiling both the contracts
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
 
+// Creating separate files to hold the compiled codes of the two contracts
 for (let contract in output) {
   fs.outputJsonSync(
     path.resolve(buildPath, contract.replace(':', '') + '.json'),
