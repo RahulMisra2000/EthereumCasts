9eb4520ff6385b3928f8d5bb8116cdda4ca67077
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-24 15:42:43 -0400

    Comments

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -5,12 +5,19 @@ import Layout from '../components/Layout';
 import { Link } from '../routes';
 
 class CampaignIndex extends Component {
-  static async getInitialProps() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
+  
+  // next.js (like Angular UNIVERSAL to do SSR, except it has a lot of other features)
+  // It calls this lifecycle method *** during SSR *** and so in this function we can get the data from the contract
+  // and return it. next.js takes the returned "stuff" and INJECTS it into the props property of this component (this.props. ) so that 
+  // it can be accessed by any function in this component.
+  static async getInitialProps() { 
+    const campaigns = await factory.methods.getDeployedCampaigns().call();  // returns an array of addresses ...
 
-    return { campaigns };
+    return { campaigns };   // ES6 feature .. this is same as  return { campaigns : campaigns };
   }
 
+
+  
   renderCampaigns() {
     const items = this.props.campaigns.map(address => {
       return {
@@ -34,17 +41,11 @@ class CampaignIndex extends Component {
           <h3>Open Campaigns</h3>
 
           <Link route="/campaigns/new">
-            <a>
-              <Button
-                floated="right"
-                content="Create Campaign"
-                icon="add circle"
-                primary
-              />
-            </a>
+            <a> <Button floated="right" content="Create Campaign" icon="add circle"  primary  />  </a>
           </Link>
 
-          {this.renderCampaigns()}
+          {this.renderCampaigns()}    // Inside the render(), all javascript stuff has to be placed inside {  }
+                                      // that is why the renderCampaigns() method call is placed inside it 
         </div>
       </Layout>
     );
