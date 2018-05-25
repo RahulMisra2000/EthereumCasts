3bdf27d9eead31b2272d2092136eb70d0874b9fa
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-05-25 14:03:58 -0400

    Comments

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -11,21 +11,23 @@ class ContributeForm extends Component {
     loading: false
   };
 
-  onSubmit = async event => {
-    event.preventDefault();
+  onSubmit = async event => {     // has to be marked with async because we are using await inside this function
+    event.preventDefault();       // we don't want any postback to happen since we are handling everything with submission
 
     const campaign = Campaign(this.props.address);
 
-    this.setState({ loading: true, errorMessage: '' });
+    this.setState({ loading: true, errorMessage: '' });   // setting loading to true so that animation starts to give users
+    // feedback because we are creating and sending a Transaction object (.send()) because we are calling setter methods in the contract 
+    // and they take time 30 seconds+ to finish .... so FEEDBACK is CRITICAL ***********
 
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.contribute().send({
-        from: accounts[0],
-        value: web3.utils.toWei(this.state.value, 'ether')
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
       });
 
-      Router.replaceRoute(`/campaigns/${this.props.address}`);
+      Router.replaceRoute(`/campaigns/${this.props.address}`);    // Programmatic Redirect
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
@@ -35,6 +37,12 @@ class ContributeForm extends Component {
 
   render() {
     return (
+      // We are using the semantic-ui-react 3rd party librarie's ui widgets and error ... loading etc ... are all features it offers
+      // basically if the error property on the <Form widget is true then the <Message element displays  
+      // Similarly, that libraries Button widget has a loading property .. if it is set to true then it does animation and if 
+      // it is false then, it shows just the text between <Button> and  </Button>
+      // That is just how this 3rd party libraries' widget work ...
+      
       <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
